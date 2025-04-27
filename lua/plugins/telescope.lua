18bbfe2825506a923c49c4dc6dff0fc31f95c65e
Geet Patel <geetpofficial@gmail.com>
2025-04-27 19:23:54 -0400

    Removing telescope commented lines

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,4 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" }
-    --config = function()
-        --require("telescope").setup {
-            --your Telescope config here
-        --}
-    --end,
 }
