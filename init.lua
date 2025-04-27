7ea3df0b8418c2d2ead1b2f0986520eff3332abe
Geet Patel <geetpofficial@gmail.com>
2025-04-27 19:47:36 -0400

    Adding live grep with args

--- init.lua
+++ init.lua
@@ -17,9 +17,11 @@ vim.g.mapleader = " "
 
 require('config.lazy')
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+
+local telescope_extensions = require('telescope').extensions
+vim.keymap.set('n', '<leader>fg', telescope_extensions.live_grep_args.live_grep_args, { desc = 'Telescope live grep with args' })
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,12 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = {
+        { "nvim-lua/plenary.nvim" },
+        { "nvim-telescope/telescope-live-grep-args.nvim" }
+    },
+    config = function()
+        local telescope = require('telescope')
+
+        telescope.load_extension('live_grep_args')
+    end
 }
