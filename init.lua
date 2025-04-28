925dba3a0e11ffc453ea3b7fa2947fa886f63a74
Geet Patel <geetpofficial@gmail.com>
2025-04-28 01:21:41 -0400

    yank, nohl, write helper

--- init.lua
+++ init.lua
@@ -15,9 +15,26 @@ vim.opt.clipboard = 'unnamedplus'
 -- leader
 vim.g.mapleader = " "
 
-require('config.lazy')
-
+-- un-highlight search
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- remove trailing spaces and lines
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*',
+    callback = function()
+        vim.cmd([[%s/\s\+$//e]])
+        vim.cmd([[%s/\($\n\s*\)\+\%$//e]])
+    end
+})
+
+-- highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
+
+require('config.lazy')
 
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
@@ -26,4 +43,3 @@ vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telesco
 
 local telescope_extensions = require('telescope').extensions
 vim.keymap.set('n', '<leader>fg', telescope_extensions.live_grep_args.live_grep_args, { desc = 'Telescope live grep with args' })
-
