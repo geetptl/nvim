508b071da33ade9442c51b6fc2e6ee55a1ddce32
Geet Patel <geetpofficial@gmail.com>
2025-04-28 17:25:23 -0400

    Telescope config moved

--- init.lua
+++ init.lua
@@ -35,11 +35,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 require('config.lazy')
-
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
-
-local telescope_extensions = require('telescope').extensions
-vim.keymap.set('n', '<leader>fg', telescope_extensions.live_grep_args.live_grep_args, { desc = 'Telescope live grep with args' })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,18 @@ return {
     },
     config = function()
         local telescope = require('telescope')
+        local builtin = require('telescope.builtin')
+        local extensions = telescope.extensions
 
         telescope.load_extension('live_grep_args')
+
+        local map = vim.keymap.set
+        local opts = { noremap = true, silent = true }
+
+        map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files', unpack(opts) })
+        map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers', unpack(opts) })
+        map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags', unpack(opts) })
+
+        map('n', '<leader>fg', extensions.live_grep_args.live_grep_args, { desc = 'Telescope live grep with args', unpack(opts) })
     end
 }
